1a7f697d9c332a9c2db7ed776f991d6d36ad7971
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-12 15:30:32 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,31 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+def find_item_by_name_in_collection(name, collection)
+  collection.map do |element|
+    element.map do |key, value|
+      if name == value
+        return element
+      end
+      end
+    end
+
+  return nil
 end
 
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
+def consolidate_cart(cart)
+  output = []
+
+  cart.each do |element|
+    if output.include?(element)
+      element[:count] += 1
+    else
+      element[:count] = 1
+      output.push(element)
+    end
+
+  end
+  output
 end
 
 
